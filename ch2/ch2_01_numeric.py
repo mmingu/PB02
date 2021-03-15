1a9de7332fe953510590c47d6b845027e5cb4a8e
alsrn36533 <80623102+alsrn36533@users.noreply.github.com>
2021-03-15 17:52:47 +0900

    Add files via upload

---
+++ ch2/ch2_01_numeric.py
@@ -0,0 +1,45 @@
+# ch2_01_numeric.py
+#
+print("숫자형: 정수")
+a = 123
+print(a)
+a = -178
+print(a)
+a = 0
+print(a)
+
+print("숫자형: 실수")
+a = 1.2
+print(a)
+a = -3.48
+print(a)
+a = 4.24e10
+print(a)
+a = 4.24e-10
+print(a)
+print("숫자형: 8진수와 16진수")
+a = 0o177
+a
+print(a)
+a = 0x8FF
+a
+print(a)
+a = 0xABC
+a
+print(a)
+print("숫자형: 연산자와 연산")
+a = 3
+b = 4
+print(a + b)
+print(a / b)
+print(a ** b)
+print(a % b)
+print(a // b)
+
+print(14 // 3)
+14 % 3
+
+"""
+Author: redwoods
+파이썬 코드: ch2_01_numeric.py
+"""
